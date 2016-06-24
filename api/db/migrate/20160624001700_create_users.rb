df133c3999dfcc0ec7b58fa46d46fac27318d198
Hank Chen & Robin Soubry & Andrew Lee <pair+Hank860502.RobinSoubry.adamandrew@devbootcamp.com>
2016-06-23 17:20:17 -0700

    write migration for user

--- api/db/migrate/20160624001700_create_users.rb
+++ api/db/migrate/20160624001700_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :email
 
       t.timestamps null: false
     end
