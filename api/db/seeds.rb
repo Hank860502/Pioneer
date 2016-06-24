7dd5ab8c7dd45d4fe0286f3ada734d12b06916a2
Andrew Lee <email": null>
2016-06-24 16:19:02 -0700

    add seed to test

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+5.times do
+  User.create(username: Faker::Internet.user_name, password: "123", email: Faker::Internet.email)
+end
+
+10.times do
+  Wishlist.create(user_id: User.all.sample.id, description: Faker::Lorem.sentence)
+end
+
+20.times do
+  Place.create(name: Faker::University.name, description: Faker::Lorem.sentence, image_url: Faker::Avatar.image, suggest_time: rand(5), price: rand * 100, longitude: rand * 100, latitude: rand * 100)
+
+end
+
+50.times do
+  PlacesWishlist.create(place_id: Place.all.sample.id, wishlist_id: Wishlist.all.sample.id, liked: [true, false].sample)
+end
