e42554430b1380afcd0fba0bd4b30194e7b60b48
Robin Soubry <robin.soubry@gmail.com>
2016-06-25 11:49:54 -0700

    Setup entry scene with navigation

--- Pioneer/index.ios.js
+++ Pioneer/index.ios.js
@@ -9,45 +9,103 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Navigator,
+  TouchableHighlight,
+  TouchableOpacity,
 } from 'react-native';
 
+import Main from './Main.js';
+import Card from './Card.js';
+
+var NavigationBarRouteMapper = {
+  LeftButton(route, navigator, index, navState) {
+    if(index > 0) {
+      return (
+        <TouchableHighlight
+        	 underlayColor="transparent"
+           onPress={() => { if (index > 0) { navigator.pop() } }}>
+          <Text style={ styles.leftNavButtonText }>Back</Text>
+        </TouchableHighlight>
+  	)}
+  	else { return null }
+  },
+  RightButton(route, navigator, index, navState) {
+    if (route.onPress) return ( <TouchableHighlight
+    														onPress={ () => route.onPress() }>
+                                <Text style={ styles.rightNavButtonText }>
+                                  	{ route.rightText || 'Right Button' }
+                                </Text>
+                              </TouchableHighlight> )
+  },
+  Title(route, navigator, index, navState) {
+    return <Text style={ styles.title }>Pioneer</Text>
+  }
+};
+
+
+
 class Pioneer extends Component {
+
+  renderScene(route, navigator){
+    if(route.title === 'Pioneer'){
+      return <Main navigator={navigator} />
+    } else if (route.title === 'Card') {
+      return <Card navigator={navigator} {...route.passProps} />
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+        style={{flex:1}}
+        initialRoute={{title: 'Pioneer'}}
+        renderScene={ this.renderScene.bind(this) }
+        navigationBar={
+          <Navigator.NavigationBar
+            style={ styles.nav }
+            routeMapper={NavigationBarRouteMapper}
+          />
+        }
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  mainContainer: {
+    	flex: 4,
+      flexDirection: 'column',
+      marginTop:100
+    },
+    leftNavButtonText: {
+    	fontSize: 18,
+      marginLeft:13,
+      marginTop:2
+    },
+    rightNavButtonText: {
+    	fontSize: 18,
+      marginRight:13,
+      marginTop:2
+    },
+    nav: {
+    	height: 60,
+      backgroundColor: '#efefef'
+    },
+    title: {
+    	marginTop:4,
+      fontSize:16
+    },
+    button: {
+    	height:60,
+      marginBottom:10,
+      backgroundColor: '#efefef',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    buttonText: {
+    	fontSize:18
+    },
 });
 
 AppRegistry.registerComponent('Pioneer', () => Pioneer);
