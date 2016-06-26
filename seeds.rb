7a25a010435a4d47553192253f5d395846d44ac7
Robin Soubry <robin.soubry@gmail.com>
2016-06-25 23:28:24 -0700

    Add seed file with first test elements

---
+++ seeds.rb
@@ -0,0 +1,13 @@
+alcatraz = Place.create(
+  name: "Alcatraz",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/11311457_1435769073419954_921661701_n.jpg"
+  )
+
+aquarium_of_the_bay = Place.create(
+  name: "Aquarium of the Bay",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/11820625_156774028000203_656280027_n.jpg"
+  )
+
+sutro_tower = Place.create(
+  name: "Sutro Tower",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12093482_626976757445643_1739892906_n.jpg")
