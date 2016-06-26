a3177b7ee172e9c224799ff7e1fbafa056172166
Robin Soubry <robin.soubry@gmail.com>
2016-06-25 23:39:17 -0700

    Add more places to seeds file

--- seeds.rb
+++ seeds.rb
@@ -10,4 +10,60 @@ aquarium_of_the_bay = Place.create(
 
 sutro_tower = Place.create(
   name: "Sutro Tower",
-  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12093482_626976757445643_1739892906_n.jpg")
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12093482_626976757445643_1739892906_n.jpg"
+  )
+
+cable_car = Place.create(
+  name: "Cable Car",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12135272_895455643886085_1883463243_n.jpg"
+  )
+
+muir_woods = Place.create(
+  name: "Muir Woods National Monument",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/13092278_1156458994386146_1264682601_n.jpg"
+  )
+
+sf_moma = Place.create(
+  name: "SFMOMA San Francisco Museum of Modern Art",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/13109081_1723896514559583_614709634_n.jpg"
+  )
+
+battery_spencer = Place.create(
+  name: "Battery Spencer",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/sh0.08/e35/p750x750/12724618_1571822593147746_505749575_n.jpg"
+  )
+
+china_town = Place.create(
+  name: "China Town",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/1516888_166301770399020_702906097_n.jpg"
+  )
+
+alamo_square = Place.create(
+  name: "Alamo Square",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12751408_259692627695424_311922490_n.jpg"
+  )
+
+civic_center = Place.create(
+  name: "Civic Center",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12960174_1685985931618996_1337015684_n.jpg"
+  )
+
+lombard_street = Place.create(
+  name: "Lombard Street",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/12912355_267495800257880_1905434648_n.jpg"
+  )
+
+castro_district = Place.create(
+  name: "Castro District",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/13397451_285829265095213_2012320115_n.jpg"
+  )
+
+palace_of_fine_arts = Place.create(
+  name: "Palace of Fine Arts",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/11347790_1166531286695489_2021504333_n.jpg"
+  )
+
+transamerica_tower = Place.create(
+  name: "Trans-america Tower",
+  image_url: "https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/sh0.08/e35/p750x750/12825796_528342577326118_1262316311_n.jpg"
+  )
