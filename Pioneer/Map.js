c6ec678c71b955adcd6666195cb41692d4cb3161
dbcbrianchung <brianjaychung@gmail.com>
2016-06-27 16:06:38 -0700

    map location display

--- Pioneer/Map.js
+++ Pioneer/Map.js
@@ -12,20 +12,37 @@ import {
   MapView
 } from 'react-native';
 
+// var region = {
+//   latitude,
+//   longitude,
+//   latitudeDelta,
+//   longitudeDelta,
+// };
 class MyMap extends Component {
   constructor(){
     super();
     this.state = {
-      markers: [{
-        latitude: 37.7824,
-        longitude: -122.3995,
-        title: 'learn surfing'
-      },{
-        latitude: 33.7824,
-        longitude: -120.3995,
-        title: 'learn something'
-      }]
-    };
+      lat: 33,
+      lng:-137,
+      latitudeDelta: 50,
+      longitudeDelta: 50,
+    }
+  }
+  componentWillMount(){
+    var latitude = this.props.collection.map(function(e){
+      return e.latitude
+    });
+    var longitude = this.props.collection.map(function(e){
+      return e.longitude
+    });
+
+    var maxLat = Math.max(...latitude);
+    var minLat = Math.min(...latitude);
+    var maxLng = Math.max(...longitude);
+    var minLng = Math.min(...longitude);
+
+    this.state.lat = (maxLat+minLat) /2
+    this.state.lng = (maxLng+minLng) /2
   }
 
   render(){
@@ -36,7 +53,13 @@ class MyMap extends Component {
       </Text>
         <MapView
           style={styles.map}
-          showsUserLocation={true}
+          region={{
+            latitude: this.state.lat,
+            longitude: this.state.lng,
+            latitudeDelta: .04,
+            longitudeDelta: .04,
+          }}
+          //showsUserLocation={true}
           //followUserLocation={true}
           annotations={this.props.collection}
         />
