c46a3e9f8732e886cb43a6f4f3420d00e75e608d
dbcbrianchung <brianjaychung@gmail.com>
2016-06-27 14:00:33 -0700

    pulling

--- Pioneer/api.js
+++ Pioneer/api.js
@@ -4,7 +4,7 @@ var apiKey = 'AIzaSyCj9yUP6BgnHAX-qFkkEQDmgce9hB_vpuo';
 
 var api = {
   getPlaces(lng,lat){
-    var url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&rankby=prominence&radius=500&types=bar&key=${apiKey}`;
+    var url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&rankby=prominence&radius=500&types=food&key=${apiKey}`;
     return fetch(url).then((response) => response.json()); // promise
   },
 };
