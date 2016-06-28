a611592c704b82bf12ca0be15510490309babb57
Hank860502 <hank860502@gmail.com>
2016-06-28 15:50:03 -0700

    disable nav button when user is on that page

--- Pioneer/index.ios.js
+++ Pioneer/index.ios.js
@@ -39,6 +39,9 @@ var NavigationBarRouteMapper = {
   	else { return null }
   },
   RightButton(route, navigator, index, navState) {
+    if(route.title==='Wishlist') {
+      return null
+    } else {
     return (
       <TouchableOpacity
         underlayColor="transparent"
@@ -54,22 +57,32 @@ var NavigationBarRouteMapper = {
            <Image source={require('./wishlist.png')}/>
          </View>
       </TouchableOpacity>
-  )},
+    )}
+  },
   Title(route, navigator, index, navState) {
-    return (
-      <TouchableOpacity
-        underlayColor="transparent"
-        onPress={() => { navigator.push({
-            title: 'Pioneer',
-          })
-        }}
-      >
+    if(route.title==='Pioneer'){
+      return (
         <Image
           style = {styles.image}
           source={require('./Pioneer.png')}
         />
-      </TouchableOpacity>
-    )},
+      )
+    } else{
+      return (
+        <TouchableOpacity
+          underlayColor="transparent"
+          onPress={() => { navigator.push({
+              title: 'Pioneer',
+            })
+          }}
+        >
+          <Image
+            style = {styles.image}
+            source={require('./Pioneer.png')}
+          />
+        </TouchableOpacity>
+      )}
+    }
 };
 
 class Pioneer extends Component {
