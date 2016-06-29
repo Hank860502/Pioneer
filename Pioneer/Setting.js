729581e1e8fab95cd9989657fefa445f97bcce13
Hank860502 <hank860502@gmail.com>
2016-06-29 11:13:50 -0700

    try to hide picker and show onPress button

--- Pioneer/Setting.js
+++ Pioneer/Setting.js
@@ -21,23 +21,31 @@ class Setting extends Component {
       newPlace: "",
     }
   }
+
+  appendPicker(){
+    return(
+      <Picker
+        selectedValue={this.state.category}
+        onValueChange={(category) => this.setState({category: category})}>
+        <Picker.Item label="Popular" value="tourist_destination" />
+        <Picker.Item label="Restaurant" value="restaurant" />
+        <Picker.Item label="Aquarium" value="aquarium" />
+        <Picker.Item label="Casino" value="casino" />
+        <Picker.Item label="Museum" value="museum" />
+        <Picker.Item label="Night Life" value="night_club" />
+        <Picker.Item label="Park" value="park" />
+        <Picker.Item label="Zoo" value="zoo" />
+        <Picker.Item label="Attractions" value="place_of_interset" />
+      </Picker>
+    )
+  }
   render(){
     return(
       <View>
         <Text style={styles.text}>This is the category: {this.state.category}</Text>
-        <Picker
-          selectedValue={this.state.category}
-          onValueChange={(category) => this.setState({category: category})}>
-          <Picker.Item label="Popular" value="tourist_destination" />
-          <Picker.Item label="Restaurant" value="restaurant" />
-          <Picker.Item label="Aquarium" value="aquarium" />
-          <Picker.Item label="Casino" value="casino" />
-          <Picker.Item label="Museum" value="museum" />
-          <Picker.Item label="Night Life" value="night_club" />
-          <Picker.Item label="Park" value="park" />
-          <Picker.Item label="Zoo" value="zoo" />
-          <Picker.Item label="Attractions" value="place_of_interset" />
-        </Picker>
+        <TouchableOpacity>
+          
+        </TouchableOpacity>
         <Text>This is the second: {this.state.category}</Text>
       </View>
     )
