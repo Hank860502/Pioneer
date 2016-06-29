b51ec5c2d819850acddf7f4cff33e80a1beeca2a
Hank860502 <hank860502@gmail.com>
2016-06-29 14:40:59 -0700

    change toggle display picker to fixed displaying

--- Pioneer/Setting.js
+++ Pioneer/Setting.js
@@ -17,28 +17,103 @@ class Setting extends Component {
     super(props);
     this.state = {
       category: "",
-      radius: "", // destination lat lng
+      radius: "",
       newPlace: "",
       showCancel: false,
+      showCancel2: false,
     }
   }
 
-  toggleCancel(){
-       this.setState({
-           showCancel: !this.state.showCancel,
-
-       });
-       console.log(this.state.showCancel)
-   }
-
-  appendCategoryPicker(){
-    console.log("click")
-    if (this.state.showCancel){
+  // toggleCancel(){
+  //   this.setState({
+  //     showCancel: !this.state.showCancel,
+  //   });
+  //  }
+  //
+  // toggleCancel2(){
+  //       this.setState({
+  //         showCancel2: !this.state.showCancel2,
+  //       });
+  //   }
+  //
+  // appendCategoryPicker(){
+  //   console.log("click")
+  //   if (this.state.showCancel){
+  //   return(
+  //     <View>
+  //       <Picker
+  //         selectedValue={this.state.category}
+  //         onValueChange={(category) => this.setState({category: category})}>
+  //         <Picker.Item label="Popular" value="tourist_destination" />
+  //         <Picker.Item label="Restaurant" value="restaurant" />
+  //         <Picker.Item label="Aquarium" value="aquarium" />
+  //         <Picker.Item label="Casino" value="casino" />
+  //         <Picker.Item label="Museum" value="museum" />
+  //         <Picker.Item label="Night Life" value="night_club" />
+  //         <Picker.Item label="Park" value="park" />
+  //         <Picker.Item label="Zoo" value="zoo" />
+  //         <Picker.Item label="Attractions" value="place_of_interset" />
+  //       </Picker>
+  //     </View>)
+  //   } else {
+  //       return null
+  //     }
+  // }
+  //
+  // appendRadiusPicker(){
+  //   console.log("click")
+  //   if (this.state.showCancel2){
+  //   return(
+  //     <View><Picker
+  //         selectedValue={this.state.radius}
+  //         onValueChange={(radius) => this.setState({radius: radius})}>
+  //         <Picker.Item label="1 miles" value="10" />
+  //         <Picker.Item label="5 miles" value="30" />
+  //         <Picker.Item label="10 miles" value="50" />
+  //         <Picker.Item label="20 miles" value="20" />
+  //         <Picker.Item label="30 miles" value="80" />
+  //       </Picker>
+  //     </View>
+  //       )
+  //   } else {
+  //       return null
+  //     }
+  // }
+  render(){
     return(
+      <View>
+        {/*<Text style={styles.text}>Category: {this.state.category}</Text>
+          <TouchableOpacity
+              onPress={() => this.toggleCancel()}
+              style={styles.button}>
+                <Text style={styles.buttonText}>Choose Category</Text>
+          </TouchableOpacity>
+          {this.appendCategoryPicker()}
+        </View>
+        <View>
+          <TouchableOpacity
+            onPress={() => this.toggleCancel2()}
+            style={styles.button2}>
+              <Text style={styles.buttonText}>Choose Radius</Text>
+          </TouchableOpacity>
+          {this.appendRadiusPicker()}
+        </View>*/}
+        <View>
+        <Picker
+          selectedValue={this.state.radius}
+          onValueChange={(radius) => this.setState({radius: radius})}>
+          <Picker.Item label="1 miles" value="10" />
+          <Picker.Item label="5 miles" value="30" />
+          <Picker.Item label="10 miles" value="50" />
+          <Picker.Item label="20 miles" value="20" />
+          <Picker.Item label="30 miles" value="80" />
+        </Picker>
+        </View>
+        <Text style={styles.text}>Radius</Text>
       <View>
         <Picker
           selectedValue={this.state.category}
-          onValueChange={(category) => this.setState({category: category})}>
+          onValueChange={(category) => this.setState({category: category})} mode="dialog">
           <Picker.Item label="Popular" value="tourist_destination" />
           <Picker.Item label="Restaurant" value="restaurant" />
           <Picker.Item label="Aquarium" value="aquarium" />
@@ -49,27 +124,9 @@ class Setting extends Component {
           <Picker.Item label="Zoo" value="zoo" />
           <Picker.Item label="Attractions" value="place_of_interset" />
         </Picker>
-      </View>)
-    } else {
-        return null
-      }
-  }
-  render(){
-    return(
-      <View>
-        <Text style={styles.text}>Category: {this.state.category}</Text>
-        <TouchableOpacity
-            onPress={() => this.toggleCancel()}
-            style={styles.button}>
-              <Text style={styles.buttonText}>Choose Category</Text>
-        </TouchableOpacity>
-        {this.appendCategoryPicker()}
-        <TouchableOpacity
-            onPress={() => this.toggleCancel()}
-            style={styles.button}>
-              <Text style={styles.buttonText}>Choose Radius</Text>
-        </TouchableOpacity>
-        {this.appendRadiusPicker()}
+      </View>
+      <Text style={styles.text}>Categories</Text>
+
       </View>
     )
   }
@@ -78,29 +135,15 @@ class Setting extends Component {
 
 const styles = StyleSheet.create({
   text: {
-    marginTop: 100,
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    fontSize: 20,
-  },
-  buttonText: {
-    fontSize: 16,
-    color: 'white',
-    alignSelf: 'center',
-  },
-  button: {
-    position: 'absolute',
-    left: 30,
-    top: 290,
-    height: 45,
-    width: 320,
-    borderRadius: 10,
-    flexDirection: 'row',
-    backgroundColor: '#30ABBD',
-    alignSelf: 'stretch',
+    flex: 1,
+    width: 385,
+    height: 35,
+    borderRadius: 8,
+    paddingLeft: 150,
     justifyContent: 'center',
-    marginTop: 60,
+    alignSelf: 'center',
+    fontSize: 20,
+    backgroundColor: '#30ABBD'
   },
 })
 
