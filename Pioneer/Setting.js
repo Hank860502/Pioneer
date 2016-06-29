a16f8cb60090b4f6411d50dae6501903e8ebcdb1
Hank860502 <hank860502@gmail.com>
2016-06-29 11:09:19 -0700

    change text input to picker, add all the type of categories in seed in

--- Pioneer/Setting.js
+++ Pioneer/Setting.js
@@ -8,7 +8,8 @@ import {
   Image,
   Navigator,
   TouchableOpacity,
-  TextInput
+  TextInput,
+  Picker,
 } from 'react-native';
 
 class Setting extends Component {
@@ -23,13 +24,20 @@ class Setting extends Component {
   render(){
     return(
       <View>
-        <Text style={styles.text}>HIsdfsdf</Text>
-        <Text>This is the category: {this.state.category}</Text>
-        <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-          onChangeText={(category) => this.setState({category})}
-          value={this.state.category}
-        />
+        <Text style={styles.text}>This is the category: {this.state.category}</Text>
+        <Picker
+          selectedValue={this.state.category}
+          onValueChange={(category) => this.setState({category: category})}>
+          <Picker.Item label="Popular" value="tourist_destination" />
+          <Picker.Item label="Restaurant" value="restaurant" />
+          <Picker.Item label="Aquarium" value="aquarium" />
+          <Picker.Item label="Casino" value="casino" />
+          <Picker.Item label="Museum" value="museum" />
+          <Picker.Item label="Night Life" value="night_club" />
+          <Picker.Item label="Park" value="park" />
+          <Picker.Item label="Zoo" value="zoo" />
+          <Picker.Item label="Attractions" value="place_of_interset" />
+        </Picker>
         <Text>This is the second: {this.state.category}</Text>
       </View>
     )
