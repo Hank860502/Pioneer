3a0d6ab98f42cf73b28899a6d5b4733a16c18d0f
Hank860502 <hank860502@gmail.com>
2016-06-29 10:51:40 -0700

    add first inputText for category

--- Pioneer/Setting.js
+++ Pioneer/Setting.js
@@ -8,12 +8,30 @@ import {
   Image,
   Navigator,
   TouchableOpacity,
+  TextInput
 } from 'react-native';
 
 class Setting extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      category: "",
+      radius: "", // destination lat lng
+      newPlace: "",
+    }
+  }
   render(){
     return(
-      <Text style={styles.text}>HIsdfsdf</Text>
+      <View>
+        <Text style={styles.text}>HIsdfsdf</Text>
+        <Text>This is the category: {this.state.category}</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(category) => this.setState({category})}
+          value={this.state.category}
+        />
+        <Text>This is the second: {this.state.category}</Text>
+      </View>
     )
   }
 };
@@ -21,7 +39,10 @@ class Setting extends Component {
 
 const styles = StyleSheet.create({
   text: {
-    marginTop:100,
+    marginTop: 100,
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
     fontSize: 20,
   }
 })
