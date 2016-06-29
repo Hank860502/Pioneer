b4a1eed5bb4c18ffbba6a74749850fdb75e35bbd
Hank860502 <hank860502@gmail.com>
2016-06-29 11:50:50 -0700

    add two function for hiding and showing categiry

--- Pioneer/Setting.js
+++ Pioneer/Setting.js
@@ -19,32 +19,61 @@ class Setting extends Component {
       category: "",
       radius: "", // destination lat lng
       newPlace: "",
+      showCancel: false,
     }
   }
 
+  toggleCancel(){
+       this.setState({
+           showCancel: !this.state.showCancel
+       });
+   }
+
+   _renderCancel() {
+       if (this.state.showCancel) {
+           return (
+               <TouchableHighlight
+                   onPress={this.toggleCancel()}>
+                   <View>
+                       <Text style={styles.cancelButtonText}>Cancel</Text>
+                   </View>
+               </TouchableHighlight>
+           );
+       } else {
+           return null;
+       }
+   },
+
   appendPicker(){
+    console.log("click")
     return(
-      <Picker
-        selectedValue={this.state.category}
-        onValueChange={(category) => this.setState({category: category})}>
-        <Picker.Item label="Popular" value="tourist_destination" />
-        <Picker.Item label="Restaurant" value="restaurant" />
-        <Picker.Item label="Aquarium" value="aquarium" />
-        <Picker.Item label="Casino" value="casino" />
-        <Picker.Item label="Museum" value="museum" />
-        <Picker.Item label="Night Life" value="night_club" />
-        <Picker.Item label="Park" value="park" />
-        <Picker.Item label="Zoo" value="zoo" />
-        <Picker.Item label="Attractions" value="place_of_interset" />
-      </Picker>
+      <View>
+        <Picker
+          selectedValue={this.state.category}
+          onValueChange={(category) => this.setState({category: category})}>
+          <Picker.Item label="Popular" value="tourist_destination" />
+          <Picker.Item label="Restaurant" value="restaurant" />
+          <Picker.Item label="Aquarium" value="aquarium" />
+          <Picker.Item label="Casino" value="casino" />
+          <Picker.Item label="Museum" value="museum" />
+          <Picker.Item label="Night Life" value="night_club" />
+          <Picker.Item label="Park" value="park" />
+          <Picker.Item label="Zoo" value="zoo" />
+          <Picker.Item label="Attractions" value="place_of_interset" />
+        </Picker>
+      </View>
     )
   }
   render(){
+    var pickerCategory = this.appendPicker()
     return(
       <View>
         <Text style={styles.text}>This is the category: {this.state.category}</Text>
-        <TouchableOpacity>
-          
+        <TouchableOpacity
+          style={styles.button}
+          onPress={pickerCategory}
+          underlayColor= '#40B7DB' >
+          <Text style={styles.buttonText}>Search By Category</Text>
         </TouchableOpacity>
         <Text>This is the second: {this.state.category}</Text>
       </View>
@@ -60,7 +89,25 @@ const styles = StyleSheet.create({
     // justifyContent: 'center',
     // alignItems: 'center',
     fontSize: 20,
-  }
+  },
+  buttonText: {
+    fontSize: 16,
+    color: 'white',
+    alignSelf: 'center',
+  },
+  button: {
+    position: 'absolute',
+    left: 30,
+    top: 415,
+    height: 45,
+    width: 320,
+    borderRadius: 10,
+    flexDirection: 'row',
+    backgroundColor: '#30ABBD',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    marginTop: 60,
+  },
 })
 
 export default Setting;
