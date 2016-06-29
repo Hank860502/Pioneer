6d76a4f54a069dc6f8e14aca588c0cfe2d24e2e0
Robin Soubry <robin.soubry@gmail.com>
2016-06-29 15:50:48 -0700

    Revert "Bc detailspage"

--- Pioneer/DetailImages.js
+++ Pioneer/DetailImages.js
@@ -15,49 +15,28 @@ import {
 
 class DetailImages extends Component {
 
-  showImages() {
-    console.log(this.props.photos)
-    var components = this.props.photos.map((photo,index)=>{
-    var photo = photo.replace(/^http:\/\//i, 'https://');
-      console.log(photo)
-      return (
-        <View key={index} style={styles.slide2}>
-         <Image style={styles.image} source={{uri: photo}} />
-        </View>
-      )
-    })
-    return components
-  }
-
 
   render(){
-    var images = this.showImages()
-    if(images.length == 1){
-      return(
-      <Image style={styles.image} source={{uri: this.props.photos[0]}} />
-      )
-    } else {
     return (
       <Swiper style={styles.wrapper} height={360} showsButtons={true}>
-        {images}
-         {/*<View style={styles.slide1}>
-           <Image style={styles.image} source={{uri: this.props.photos[0]}} />
-         </View>
-         <View style={styles.slide2}>
-          <Image style={styles.image} source={{uri: this.props.photos[1]}} />
-         </View>
-         <View style={styles.slide3}>
-          <Image style={styles.image} source={{uri: this.props.photos[2]}} />
-         </View>*/}
-      </Swiper>
+           <View style={styles.slide1}>
+             <Image style={styles.image} source={{uri: this.props.photos}} />
+           </View>
+           <View style={styles.slide2}>
+             <Text style={styles.text}>Addtional photos here</Text>
+           </View>
+           <View style={styles.slide3}>
+             <Text style={styles.text}>Addtional photos here</Text>
+           </View>
+         </Swiper>
     )
-    }
+
   }
 }
   const styles = StyleSheet.create({
     wrapper: {
      },
-     slide: {
+     slide1: {
        justifyContent: 'center',
        alignItems: 'center',
        backgroundColor: '#9DD6EB',
@@ -78,7 +57,7 @@ class DetailImages extends Component {
        fontWeight: 'bold',
     },
     image:{
-      height:360,
+      height:350,
       width:375,
     },
 });
