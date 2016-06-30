6b207129d26f69050a4320c51948aaaf687e51aa
Robin Soubry <robin.soubry@gmail.com>
2016-06-30 12:44:51 -0700

    Add icons for the types

--- Pioneer/Detail.js
+++ Pioneer/Detail.js
@@ -30,66 +30,212 @@ class Detail extends Component {
     }
   }
 
+  priceIndication(){
+    var price = "";
+    if(this.props.card.price_level){
+      for(i = 0 ; i < this.props.card.price_level ; i++){
+        price += "$";
+      }
+    }
+    return price;
+  }
+
+  typeIcon(type,index){
+    console.log(type)
+    switch(type) {
+      case "aquarium":
+        return (
+          <View key={index}>
+            <Image style={styles.type} source={require("./iconAquarium.png")} />
+          </View>
+        )
+        break;
+      case "bar":
+        return (
+          <View key={index}>
+          <Image style={styles.type} source={require("./iconBar.png")} />
+          </View>
+        )
+        break;
+      case "casino":
+        return (
+          <View key={index}>
+          <Image style={styles.type} source={require("./iconCasino.png")} />
+          </View>
+        )
+        break;
+      case "museum":
+        return (
+          <View key={index}>
+          <Image style={styles.type} source={require("./iconMuseum.png")} />
+          </View>
+        )
+        break;
+      case "night_club":
+        return (
+          <View key={index}>
+          <Image style={styles.type} source={require("./iconNightClub.png")} />
+          </View>
+        )
+        break;
+      case "park":
+        return (
+          <View key={index}>
+          <Image style={styles.type} source={require("./iconPark.png")} />
+          </View>
+        )
+        break;
+      case "restaurant":
+        return (
+          <View key={index}>
+          <Image style={styles.type} source={require("./iconRestaurant.png")} />
+          </View>
+        )
+        break;
+      case "zoo":
+        return (
+          <View key={index}>
+          <Image style={styles.type} source={require("./iconZoo.png")} />
+          </View>
+        )
+        break;
+      case "place_of_interest":
+        return (
+          <View key={index}>
+          <Image style={styles.type} source={require("./iconPlaceOfInterest.png")} />
+          </View>
+        )
+        break;
+      case "tourist_destination":
+        return (
+          <View key={index}>
+            <Image style={styles.type} source={require("./iconTourism.png")} />
+          </View>
+        )
+        break;
+      default:
+        return (
+          <View key={index}>
+            <Image style={styles.type} source={require("./iconDefault.png")} />
+          </View>
+        )
+    }
+    // return <Image source={require("./1Star.png")} />
+  }
+
+  renderTypes(){
+    var types = this.props.card.types
+    console.log(types);
+    var typeIcons = types.map((category,index) => {
+      return this.typeIcon(category,index)
+    });
+    console.log(typeIcons);
+    return typeIcons
+  }
+
   render(){
+    var placeTypes = this.renderTypes();
     var stars = this.starRating()
     var card = this.props.card;
+    var priceIndication = this.priceIndication()
     var url = `http://maps.apple.com/?daddr=${card.latitude},${card.longitude}`;
-    console.log(url);
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white'}}>
 
-        {/*<Image style={styles.image} source={{uri: this.props.card.photos[0]}} />*/}
+      // <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white'}}>
+
+      <ScrollView style={styles.container}>
         <DetailImages photos={card.photos}/>
-        <Text style={styles.title}>
-        {card.title}
-        </Text>
-        {/*{stars}*/}
-        <Separator />
 
+        <View style={styles.overview}>
+          <Text style={styles.title}>
+            {card.title}
+          </Text>
 
-        <Text style={styles.welcome}>
+          <Text style={styles.description}>
           {card.description}
-        </Text>
+          </Text>
+
+          {stars}
+        </View>
+
         <Separator />
 
-        <Text style={styles.welcome}>
-        {card.address}
-        </Text>
-        <Text style={{color:'blue'}}
-        onPress={()=>Linking.openURL(url)}>
-        Take Me There
-        </Text>
-        <Separator />
-        <Text style={styles.welcome}>
-          {this.props.card.price}
-        </Text>
+        <View style={styles.flexed}>
+          {placeTypes}
+        </View>
 
-      </View>
+        <View style={styles.overview}>
+
+
+          <Text style={styles.description}>
+            {priceIndication}
+          </Text>
+        </View>
+          {/*<Separator />*/}
+
+          <Text style={styles.description}>
+          {card.address}
+          </Text>
+          <Text style={{color:'blue'}}
+          onPress={()=>Linking.openURL(url)}>
+          Take Me There
+          </Text>
+
+          <Separator />
+          <Text style={styles.welcome}>
+            {this.props.card.price}
+          </Text>
+
+
+      </ScrollView>
+
+      // </View>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-     flex: 1,
-     justifyContent: 'center',
-     alignItems: 'center',
+    marginTop: 20,
      backgroundColor: 'white',
    },
-   welcome: {
-     fontSize: 20,
-     textAlign: 'center',
-     margin: 10,
-   },
+   overview: {
+      padding: 10,
+      alignItems: 'center',
+    },
    title: {
      fontSize: 20,
      textAlign: 'center',
      marginTop: 10,
    },
+   description: {
+     fontSize: 16,
+     textAlign: 'center',
+     margin: 10,
+   },
    image:{
      height:350,
      width:375,
    },
+   type: {
+      height: 25,
+      width: 25,
+      marginLeft: 5,
+      marginRight: 5,
+      marginTop: 10,
+      // flexDirection: 'row',
+      // -webkit-flex-direction: row,
+      // flex-direction: row,
+    },
+    flexed: {
+      //  height: 25,
+      //  width: 25,
+      flexDirection: 'row',
+      alignSelf: 'center',
+       // -webkit-flex-direction: row,
+       // flex-direction: row,
+     },
 });
 
 
